b8ed16a7510d120eb4efbc76d37f636809d1d1e4
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-12-11 11:44:20 +0100

    [fix] update fetch URL to relative path

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
 function fetchData() {
 	$('#fetchButton').click(function () {
-		fetch('/data.json')
+		fetch('data.json')
 			.then((response) => {
 				if (!response.ok) {
 					throw new Error('Network response was not ok');
